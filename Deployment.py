c118d7de1df25b61c463318413a8064dc5483042
Venkat Choudhary Ala <128159176+venkatchoudharyala@users.noreply.github.com>
2023-06-13 16:15:34 +0530

    Update Deployment.py

--- Deployment.py
+++ Deployment.py
@@ -19,35 +19,36 @@ def main():
   if st.button("Predict Your's"):
     # Create input fields for user to enter features
     Age = st.text_input("Age", "28")
-    Education = st.selectbox("Education Level", ["High School", "Bachelor's Degree", "Master's Degree", "Ph.D."])
+    Education = st.text_input("Education Level", "3")
     AvgDailyHours = st.text_input("Average Daily Hours", "8")
-    Department = st.selectbox("Department", ["Sales", "Marketing", "Finance", "HR", "Operations"])
-    EducationField = st.selectbox("Education Field", ["Mathematics", "Science", "Arts", "Engineering"])
+    EducationField = st.selectbox("Department", ["Technical Degree", "Medical", "Human Resources", "Marketing", "Other"])
+    Department = st.selectbox("Education Field", ["Manufacturing Director", "Sales Representative", "Healthcare Representative", "Research Director", "Manager", "Human Resources", "Sales Executive", "Laboratory Technician", "Research Scientist"])
     Gender = st.selectbox("Gender", ["Male", "Female"])
-    HasFlexibleTimings = st.selectbox("Has Flexible Timings", ["Yes", "No"])
+    HasFlexibleTimings = st.selectbox("Has Flexible Timings", ["No", "Yes"])
     IsIndividualContributor = st.selectbox("Is Individual Contributor", ["Yes", "No"])
     JobInvolvement = st.slider("Job Involvement", 1, 4, 2)
     JobRole = st.selectbox("Job Role", ["Manager", "Engineer", "Analyst", "Sales", "HR"])
     JobSatisfaction = st.slider("Job Satisfaction", 1, 5, 3)
     LeavesTaken = st.text_input("Leaves Taken", "15")
-    MaritalStatus = st.selectbox("Marital Status", ["Single", "Married", "Divorced"])
+    MaritalStatus = st.selectbox("Marital Status", ["Divorced", "Married", "Single"])
     MicromanagedAtWork = st.slider("Micromanaged at Work", 0, 10, 5)
     MonthlyIncome = st.text_input("Monthly Income", "25000")
     NumCompaniesWorked = st.text_input("Number of Companies Worked", "2")
     PercentSalaryHike = st.text_input("Percent Salary Hike", "45")
     PerformanceRating = st.slider("Performance Rating", 1, 5, 3)
     RelationshipSatisfaction = st.slider("Relationship Satisfaction", 1, 5, 3)
-    RemoteWorkSatisfaction = st.selectbox("Remote Work Satisfaction", ["Satisfied", "Neutral", "Unsatisfied"])
+    RemoteWorkSatisfaction = st.selectbox("Remote Work Satisfaction", ["Medium", "Very High", "High", "Low", "Very Low"])
     SelfMotivationLevel = st.slider("Self Motivation Level", 1, 5, 3)
     TotalWorkingYears = st.text_input("Total Working Years", "5")
     TrainingTimesLastYear = st.text_input("Training Times Last Year", "6")
     WorkLifeBalance = st.slider("Work-Life Balance", 1, 5, 3)
-    WorkLoadLevel = st.selectbox("Work Load Level", ["Low", "Medium", "High"])
+    WorkLoadLevel = st.selectbox("Work Load Level", ["Low", "High", "Medium"])
     YearsAtCompany = st.text_input("Years at Company", "2")
     YearsSinceLastPromotion = st.text_input("Years Since Last Promotion", "2")
     YearsWithCurrManager = st.text_input("Years with Current Manager", "2")
 
     UserData = pd.DataFrame({
+    'EmployeeID': [000000],
     'Age': [Age],
     'Education': [Education],
     'AvgDailyHours': [AvgDailyHours],
