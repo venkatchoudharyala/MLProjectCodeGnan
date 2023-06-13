fc70cd3ef729bc8f331049aecd5a763e2b039631
Venkat Choudhary Ala <128159176+venkatchoudharyala@users.noreply.github.com>
2023-06-13 18:25:00 +0530

    Update Deployment.py

--- Deployment.py
+++ Deployment.py
@@ -20,6 +20,7 @@ data = load_data()
 
 # Streamlit App
 @st.experimental_singleton
+@st.cache_resource(experimental_allow_widgets=True)
 def main():
     # Set the title and description of the app
     st.title("Workplace Stress Prediction")
