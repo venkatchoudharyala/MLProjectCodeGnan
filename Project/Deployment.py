ddf958fc688a64601203fb7cc69bb13aa9206f05
Venkat Choudhary Ala <128159176+venkatchoudharyala@users.noreply.github.com>
2023-06-14 12:07:25 +0530

    Update Deployment.py

--- Project/Deployment.py
+++ Project/Deployment.py
@@ -237,10 +237,19 @@ def main():
             st.pyplot()
         if "RocCurve" in Metrics:
             st.subheader("ROC Curve")
-            roc_curve_display = RocCurveDisplay.from_estimator(Model, x_test, y_test)
-            fig, ax = plt.subplots()
-            roc_curve_display.plot(ax=ax)
-            st.pyplot(fig)
+            fpr, tpr, _ = roc_curve(y_test, y_pred)
+            roc_auc = auc(fpr, tpr)
+            plt.figure()
+            plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
+            plt.plot([0, 1], [0, 1], 'k--')
+            plt.xlim([0.0, 1.0])
+            plt.ylim([0.0, 1.05])
+            plt.xlabel('False Positive Rate')
+            plt.ylabel('True Positive Rate')
+            plt.title('Receiver Operating Characteristic')
+            plt.legend(loc="lower right")
+            st.pyplot(plt.gcf())
+
         if "PrecisionRecallCurve" in Metrics:
             st.subheader("Precision-Recall Curve")
             precision, recall, _ = precision_recall_curve(y_test, y_pred)
