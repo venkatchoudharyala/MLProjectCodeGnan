0b7321225809dc8aa105c9eef20900606eda5233
Venkat Choudhary Ala <128159176+venkatchoudharyala@users.noreply.github.com>
2023-06-15 11:00:20 +0530

    Update Deployment.py

--- Project/Deployment.py
+++ Project/Deployment.py
@@ -208,7 +208,38 @@ def main():
         st.markdown("---")
 
         ModifiedData = data.copy()
-        ModifiedData.dropna(inplace = True)
+        le = LabelEncoder()
+        for column in ModifiedData.columns:
+            if ModifiedData[column].dtype == object:
+                TempVal = le.fit_transform(ModifiedData[column].astype('category'))
+                PreProModifiedData[column] = TempVal
+            else:
+                PreProModifiedData[column] = ModifiedData[column]
+        PreProModifiedData.drop("EmployeeID", axis = 1, inplace = True)
+        PreProModifiedData.dropna(inplace = True)
+        
+        data = PreProModifiedData
+
+        # Interactive Plotly graph 1: Scatter plot
+        scatter_plot = px.scatter(data, x="Age", y="MonthlyIncome", color="JobRole")
+        scatter_plot.show()
+
+        # Interactive Plotly graph 2: Bar chart
+        bar_chart = px.bar(data, x="EducationField", y="TotalWorkingYears", color="Department")
+        bar_chart.show()
+
+        # Interactive Plotly graph 3: Line plot
+        line_plot = px.line(data, x="YearsAtCompany", y="JobSatisfaction", color="MaritalStatus")
+        line_plot.show()
+
+        # Interactive Plotly graph 4: Pie chart
+        pie_chart = px.pie(data, names="Gender")
+        pie_chart.show()
+
+        # Interactive Plotly graph 5: 3D scatter plot
+        scatter_3d = px.scatter_3d(data, x="YearsWithCurrManager", y="YearsSinceLastPromotion", z="PercentSalaryHike", color="PerformanceRating")
+        scatter_3d.show()
+        
         Visu = px.scatter(ModifiedData, x='MonthlyIncome', y='JobSatisfaction', size='Age', color='Target',
                  hover_name='EmployeeID', log_x=True, title='Job Satisfaction vs Target Variable')
         st.plotly_chart(Visu)
